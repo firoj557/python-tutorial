34245ff589d5319c102094a04ba9174748607aa0
firoj557 <firojkumar557@gmail.com>
2020-04-25 19:06:42 +0530

    list program file added on repo

---
+++ list program.py
@@ -0,0 +1,42 @@
+""" Author: Firoj Kumar
+
+ Date: 25-04-2020
+
+This program list initialization!"""
+
+a=[5,50.4,10,125,'firoj',15,'umesh',20,40]
+
+print(a[5])
+
+"""output-15"""
+
+print(a[1:])
+"""output-[50.4,10,125,'firoj',15,'umesh',20,40]"""
+
+print(a[:5])
+
+"""output [5,50.4,10,125,'firoj']"""
+
+"""change the list values"""
+
+a[5]='rakesh'
+print(a[1:])
+
+a[3]=100
+print(a[1:])
+
+"""output=[50.4, 10, 100, 'firoj', 'rakesh', 'umesh', 20, 40]"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
