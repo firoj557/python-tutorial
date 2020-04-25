58ce59515e4d60433c2fd20fadfdcd3ec3e13c70
firoj557 <firojkumar557@gmail.com>
2020-04-25 19:13:54 +0530

    number data type program file added on repo

---
+++ number program.py
@@ -0,0 +1,47 @@
+"""Author: Firoj Kumar
+
+# Date: 25-04-2020
+
+# This program define number data type!"""
+
+
+a=10
+print(a, "is of type", type(a))
+
+"""output =10 is of type <class 'int'>"""
+
+b=5.5
+print(b, "is of type", type(b))
+
+"""output =5.5 is of type <class 'float'>"""
+
+
+
+c = 5+3j
+print(c, "is complex number?", isinstance(5+3j,complex))
+
+"""output=(5+3j) is complex number? True"""
+
+
+d = 1+2j
+print(d, "is complex number?", isinstance(1+2j,complex))
+
+"""output=(1+2j) is complex number? True"""
+
+
+
+a=123456789123456789123456789123456789123456789123456789123456789
+print(a)
+
+"""output= 123456789123456789123456789123456789123456789123456789123456789"""
+
+b=0.123456789123456789123456789123456789123456789123456789123
+print(b)
+
+"""output=0.12345678912345678 """
+
+
+
+c= 2+5j
+print(c)
+"""output=(2+5j)"""
