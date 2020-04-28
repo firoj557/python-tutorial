9ad4f8205387f6392819496f81c83ce65dd63733
firoj557 <firojkumar557@gmail.com>
2020-04-28 19:07:53 +0530

    set not support duble value program added on repo

---
+++ set not support duble value program.py
@@ -0,0 +1,10 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program is set not support double value !"""
+
+"""Not support dubling value"""
+x={10,20,30,40,50,'hitesh','rakesh',2.0,100,100}
+print("x=",x)
+"""output=x={10,20,30,40,50,'hitesh','rakesh',2.0,100,}"""
