ddc5d98b9f778f8aef4bddb778e5cfbdc762c08e
firoj557 <firojkumar557@gmail.com>
2020-04-28 18:41:53 +0530

    tuple print spacific element program

---
+++ tuple print spacific element.py
@@ -0,0 +1,11 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program is Tuple print spacific elements!"""
+
+t = (10,20,30,40,50,'firoj', 1+3j,10.5,90,100)
+
+"""(1)print 3rd no elements"""
+print("t[3] = ", t[3])
+"""Output t[3] = 40 """
