0158580c911374ff07669e498ab3d1273c6bdf80
firoj557 <firojkumar557@gmail.com>
2020-04-28 21:28:28 +0530

    number datatype print complex value program added on repo

---
+++ number datatype print complex value.py
@@ -0,0 +1,12 @@
+"""Author: Firoj Kumar
+
+# Date: 28-04-2020
+
+# This program define number data type!"""
+
+
+""" print complex value """
+c = 5+3j
+print(c, "is complex number?", isinstance(5+3j,complex))
+
+"""output=(5+3j) is complex number? True"""
