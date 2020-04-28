0e07afa6a3e5f4daba820ea1ed2b2c28090a4ec7
firoj557 <firojkumar557@gmail.com>
2020-04-28 23:37:44 +0530

    float conversion program added on repo

---
+++ float conversion program.py
@@ -0,0 +1,15 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program is float value conversion program !"""
+
+a=8
+b=-9
+print("a= ",float(a))
+print("b=",float(b))
+
+
+
+"""output= a= 8.0 """
+"""output= b= -9.0 """
