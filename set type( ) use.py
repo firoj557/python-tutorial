70a71c119b36fe2f3899f8d731e4762668e9832f
firoj557 <firojkumar557@gmail.com>
2020-04-28 18:08:46 +0530

    set type function program added in repo

---
+++ set type( ) use.py
@@ -0,0 +1,11 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program is set type () initialization!"""
+
+x={10,20,30,40,50,'hitesh','rakesh',2.0,100,}
+
+"""type() use on set"""
+print(x,type(x))
+"""output = <class 'set'>"""
