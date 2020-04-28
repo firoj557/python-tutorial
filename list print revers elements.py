5a702181987e7529ba64a3609de8f4127f674f4e
firoj557 <firojkumar557@gmail.com>
2020-04-28 21:23:42 +0530

    list revers print elements program added on repo

---
+++ list print revers elements.py
@@ -0,0 +1,15 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program list print elements in revers order!"""
+
+a=[5,50.4,10,125,'firoj',15,'umesh',20,40]
+
+""" print all store elements in list"""
+print(a[0:])
+"""output-[5,50.4,10,125,'firoj',15,'umesh',20,40]"""
+
+""" print revers specific elements """
+print("a[:5]=",a[:5])
+"""output a[:5]=[5,50,4,10,125,'firoj']"""
