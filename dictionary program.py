0727d1ac0a22e497cc8c2826d5cdd9c8c7056817
firoj557 <firojkumar557@gmail.com>
2020-04-28 22:31:16 +0530

    Dictionary program added on repo

---
+++ dictionary program.py
@@ -0,0 +1,17 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program is Dictionary  print  elements!"""
+
+d={1:'firoj', 2:'hitesh', 3:'vikky'}
+print(type(d))
+print("d=",d)
+print("d[1] = ", d[1])
+
+print("d[2] = ", d[2]);
+
+"""output <class 'dict' """
+"""output d={'firoj':7898763557,'hitesh':9893176635,'vikky':9993334075}"""
+"""output= d[1]= firoj"""
+"""output= d[2]= hitesh"""
