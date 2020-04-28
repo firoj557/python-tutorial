ae74c74bede6741ef292236e8795df6a35b3b4fa
firoj557 <firojkumar557@gmail.com>
2020-04-28 18:45:45 +0530

    tuple immutable program added on repo

---
+++ tuple immutable program.py
@@ -0,0 +1,69 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program is Tuple immutable program!"""
+
+t = (10,20,30,40,50,'firoj', 1+3j,10.5,90,100)
+
+"""immutable tuple elements"""
+
+t[4]=110
+print("t[4] =",t[4])
+
+"""Output= t[4]=110
+TypeError: 'tuple' object does not support item assignment"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
