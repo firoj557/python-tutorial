dc918449252c40882cca75e3e1cd68b99df8130b
firoj557 <firojkumar557@gmail.com>
2020-04-28 18:56:07 +0530

    string immutable program added on repo

---
+++ string immutable program.py
@@ -0,0 +1,12 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program is string  immutable !"""
+"""a =assign an string  """
+a='firoj kumar'
+
+""" immutable- change the string corrector """
+a[2]='n'
+print(a)
+"""output- TypeError: 'str' obj doe not support ittem assigment"""
