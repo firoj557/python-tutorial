eb7aa7138f1f5dc47d5e43bd117aa417d71477ea
firoj557 <firojkumar557@gmail.com>
2020-04-28 21:39:28 +0530

    list spacific sequance print elemnet program added on repo

---
+++ list print spacific sequance element print.py
@@ -0,0 +1,15 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program list initialization!"""
+
+a=[5,50.4,10,125,'firoj',15,'umesh',20,40]
+
+"""print all store elements in list"""
+print(a[0:])
+"""output-[5,50.4,10,125,'firoj',15,'umesh',20,40]"""
+
+"""print list in pecifice sequance elements """
+print("a[0:4]",a[0:4])
+"""output-[5,50.4,10,125]"""
