23ae4b7335671fbc14cd54bdd7d1297e326d96dd
firoj557 <firojkumar557@gmail.com>
2020-04-28 18:48:11 +0530

    string print spacific index program added on repo

---
+++ string print spacific charactor.py
@@ -0,0 +1,13 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program is string  print a spacific charactor!"""
+
+"""a =assign an string  """
+a='firoj kumar'
+
+
+"""(print spacific charactor in a string"""
+print("a[0] [4]=",a[0],a[4])
+"""output= a[0][4]=f j"""
