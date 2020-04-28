38fe8a88a738d9ede17d428f4acc137324f9c05e
firoj557 <firojkumar557@gmail.com>
2020-04-28 23:32:49 +0530

    set conversion program added on repo

---
+++ set conversion program.py
@@ -0,0 +1,12 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program is set datatype  conversion program !"""
+
+x=(10,20,30,40,50.5,70)
+
+print("x=",set(x))
+
+"""output= {70,40,10,50.5,20,30}"""
+
