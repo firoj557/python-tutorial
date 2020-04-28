5d3afd81572cfc031603535e4810c4bcd15af453
firoj557 <firojkumar557@gmail.com>
2020-04-28 18:33:35 +0530

    tuple print revers program added on repo

---
+++ tuple print revers element program.py
@@ -0,0 +1,13 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program is Tuple print revers elements!"""
+
+t = (10,20,30,40,50,'firoj', 1+3j,10.5,90,100)
+
+""" print reverce elements"""
+
+print("t[ :9] =",t[ :9])
+
+"""output t[:9 ]= (10,20,30,40,50,'firoj', (1+3j),10.5,90)"""
