500d4666ce1795f9a3765627e352e0aac079ad0a
firoj557 <firojkumar557@gmail.com>
2020-04-28 18:51:37 +0530

    string print multiple line program added on repo

---
+++ string print a multi line quote.py
@@ -0,0 +1,11 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program is string  print a multi line quote!"""
+
+
+""" print a string in multiline quote"""
+
+print(' """firoj kumar""" ')
+
---
+++ string print a single quote program.py
@@ -0,0 +1,12 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program is string  print a single quote!"""
+
+"""a =assign an string  """
+a='firoj kumar'
+
+""" print a sting  single ' ' quote"""
+print(" 'firoj kuamr' ")
+"""output = 'firoj kumar' """
