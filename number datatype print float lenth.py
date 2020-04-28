d19dc52bb7576c45646ea78cc19b19771cf85383
firoj557 <firojkumar557@gmail.com>
2020-04-28 21:35:43 +0530

    number datatype print float value lenth program added on repo

---
+++ number datatype print float lenth.py
@@ -0,0 +1,12 @@
+"""Author: Firoj Kumar
+
+# Date: 28-04-2020
+
+# This program print float number data type lenth !"""
+
+"""(6) print float value lenth """
+
+b=0.123456789123456789123456789123456789123456789123456789123
+print(b)
+
+"""output=0.12345678912345678 """
