527bc97398f6e2f3a3b132b345b4b03bbaeb22b9
firoj557 <firojkumar557@gmail.com>
2020-04-28 21:21:08 +0530

    list mutable element program added on repo

---
+++ list mutable program.py
@@ -0,0 +1,20 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program list in mutable elements!"""
+
+a=[5,50.4,10,125,'firoj',15,'umesh',20,40]
+
+"""(1) print all store elements in list"""
+print(a[0:])
+"""output-[5,50.4,10,125,'firoj',15,'umesh',20,40]"""
+
+"""(3)mutable -change the list values"""
+
+a[5]='rakesh'
+print(a[0:])
+
+"""output [5,50.4,10,125,'firoj',rakesh,'umesh',20,40]"""
+
+
