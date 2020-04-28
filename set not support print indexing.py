778f33565d78760d6fe061f7cafd57fe2802e1d1
firoj557 <firojkumar557@gmail.com>
2020-04-28 19:10:35 +0530

    set not support print indexing values program added on repo

---
+++ set not support print indexing.py
@@ -0,0 +1,12 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program is set not support indexing !"""
+
+x={10,20,30,40,50,'hitesh','rakesh',2.0,100,}
+
+""" print spacific elements"""
+print(x[1])
+""" output=TypeError: 'set' object does not support indexing"""
+
