c04188fd23e84684964b5c6dd4e67edae490c0ec
firoj557 <firojkumar557@gmail.com>
2020-04-28 21:41:14 +0530

    string print in duble quote program added on repo

---
+++ string print a dobule quote program.py
@@ -0,0 +1,9 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program is string  print a double  quote!"""
+
+""" print a string in dobule quote"""
+print(' "firoj kumar" ')
+"""output = "firoj kumar" """
