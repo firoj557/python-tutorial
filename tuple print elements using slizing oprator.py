c9098bd1603856b4c99f90cbfb9537ad907d8781
firoj557 <firojkumar557@gmail.com>
2020-04-28 18:44:18 +0530

    tuple print values using slizing oprator progran added on repo

---
+++ tuple print elements using slizing oprator.py
@@ -0,0 +1,11 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program is Tuple print spacific elements using slizing oprator!"""
+
+t = (10,20,30,40,50,'firoj', 1+3j,10.5,90,100)
+
+"""(2) print tuple specific element using slizing oprator"""
+print("t(0:2) = ", t[0:2])
+"""Output t[0:1] =(10 20)"""
