09e5d048c52dc5608e53559594afb3f37c0509d8
firoj557 <firojkumar557@gmail.com>
2020-04-28 23:36:30 +0530

    integer conversion program added on repo

---
+++ integer conversion program.py
@@ -0,0 +1,15 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program is integer value conversion program !"""
+
+a= 10.5
+b=-15.6
+
+print("a=",int(a))
+print("b=",int(b))
+
+
+"""output= a= 10 """
+"""output= b= -15 """
