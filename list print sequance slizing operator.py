2096ce64a78970669ef8a580ae83d48d0f61ca5e
firoj557 <firojkumar557@gmail.com>
2020-04-28 21:47:20 +0530

    list print element using slizing perator added on repo

---
+++ list print sequance slizing operator.py
@@ -0,0 +1,15 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program list elements print sequance initialization!"""
+
+a=[5,50.4,10,125,'firoj',15,'umesh',20,40]
+
+""" print all store elements in list"""
+print(a[0:])
+"""output-[5,50.4,10,125,'firoj',15,'umesh',20,40]"""
+
+""" print sequance next values"""
+print("a[3:]=",a[3:])
+"""output a[3:]= [125,'firoj',15,'umesh',20,40]"""
