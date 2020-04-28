6e33607a39e7a3af67b8b7acc1f236c8ad307cfd
firoj557 <firojkumar557@gmail.com>
2020-04-28 21:25:22 +0530

    list print spacific element program added on repo

---
+++ list print spacific elements.py
@@ -0,0 +1,17 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program list in mutable elements!"""
+
+a=[5,50.4,10,125,'firoj',15,'umesh',20,40]
+
+""" print all store elements in list"""
+print(a[0:])
+"""output-[5,50.4,10,125,'firoj',15,'umesh',20,40]"""
+
+
+""" print required spacific elements in list"""
+print("a[2][5][7]=",a[2],a[5],a[7])
+
+"""output= 10 rakesh 20 """
