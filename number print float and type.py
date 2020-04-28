1914e80a791938a925557df705670b8b16cadec3
firoj557 <firojkumar557@gmail.com>
2020-04-28 21:31:08 +0530

    number datatype print float and type program added on repo

---
+++ number print float and type.py
@@ -0,0 +1,11 @@
+"""Author: Firoj Kumar
+
+# Date: 28-04-2020
+
+# This program define float  number data type!"""
+
+""" print flot value and data type function"""
+b=5.5
+print(b, "is of type", type(b))
+
+"""output =5.5 is of type <class 'float'>"""
