2f59e952a3cd0fa454a66183c83e8e603841479f
firoj557 <firojkumar557@gmail.com>
2020-04-28 23:30:10 +0530

    tuple conversion program added on repo

---
+++ tuple conversion program.py
@@ -0,0 +1,11 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program is tuple datatype  conversion program !"""
+
+x = {20,40,50,70,90,100}
+print("x=",tuple(x))
+
+
+"""output= x= (100,70,40,50,80,90) """
