31519fe760d5238277a77b535d7e7ed98291a5c0
firoj557 <firojkumar557@gmail.com>
2020-04-28 18:59:00 +0530

    set not support slizing oprator program added on repo

---
+++ set not support slizing oprator.py
@@ -0,0 +1,12 @@
+""" Author: Firoj Kumar
+
+ Date: 28-04-2020
+
+This program is set not support indexing and slizing oprator  !"""
+
+x={10,20,30,40,50,'hitesh','rakesh',2.0,100,}
+
+"""Not support slizing  : opretor"""
+print(x[1:])
+print(x)
+""" output=TypeError: 'set' object does not support indexing"""
