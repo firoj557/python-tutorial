0781f35cdd7ed5dd1ac8cdec3d16072236ce1194
firoj557 <firojkumar557@gmail.com>
2020-04-29 21:39:46 +0530

    output format program added on repo

---
+++ output format program.py
@@ -0,0 +1,11 @@
+""" Author: Firoj Kumar
+
+ Date: 29-04-2020
+
+This program is output formate  program !"""
+
+a=50
+b=100
+print('vale a is {} and b is {}'.format(a,b))
+
+"""output value a is 50 and b is 100 """
