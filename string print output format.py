b203d19c7d7cda798afcfa420d2e456168518d68
firoj557 <firojkumar557@gmail.com>
2020-04-29 21:22:02 +0530

    string print output format program added on repo

---
+++ string print output format.py
@@ -0,0 +1,14 @@
+""" Author: Firoj Kumar
+
+ Date: 29-04-2020
+
+This program is output formate print strings program !"""
+
+
+print(' i love {0} and i hate {1}'.format('mango','apple'))
+
+print(' i love {1} and i hate {0}'.format('mango','apple'))
+
+
+"""output i love mango and i hate apple"""
+"""output i love apple and i hate mango"""
