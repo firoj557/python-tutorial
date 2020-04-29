efdb288946e5f96cba77bddd4428f96917a7c3f0
firoj557 <firojkumar557@gmail.com>
2020-04-29 21:29:16 +0530

    output formate print string program added on repo

---
+++ output formate print style % operator.py
@@ -0,0 +1,16 @@
+""" Author: Firoj Kumar
+
+ Date: 29-04-2020
+
+This program is output formate using % operator"""
+
+x = 12.3456789
+
+print('The value of x is %3.2f' % x)
+
+print('The value of x is %3.4f' % x)
+
+
+"""output  the value of x is =12.35"""
+"""output  the value of x is =12.3457"""
+
