77a1ba96fed6a769f6cbe4a3d635a7cf7f659dc5
firoj557 <firojkumar557@gmail.com>
2020-04-29 21:12:04 +0530

    comparision program added on repo

---
+++ comparison oprator program.py
@@ -0,0 +1,18 @@
+""" Author: Firoj Kumar
+
+ Date: 29-04-2020
+
+This program is Comparison oprators  program !"""
+
+x= int(input("enter value of x="))
+
+y= int(input("enter value of y="))
+
+print('x > y is',x>y)
+print('x < y is',x<y)
+print('x != y is',x!=y)
+print('x >= y is',x>=y)
+print('x <= y is',x<=y)
+
+
+
