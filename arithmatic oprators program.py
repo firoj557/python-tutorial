e149bd89c99fe56dee127ed7e989ee45dadb2cd0
firoj557 <firojkumar557@gmail.com>
2020-04-29 21:06:13 +0530

    arithmatic oprator program added on repo

---
+++ arithmatic oprators program.py
@@ -0,0 +1,26 @@
+""" Author: Firoj Kumar
+
+ Date: 29-04-2020
+
+This program is Arithmatic oprator  program !"""
+
+x= int(input("enter value of x="))
+
+y= int(input("enter value of y="))
+
+print('x + y =',x+y)
+print('x - y =',x-y)
+print('x * y =',x*y)
+print('x / y =',x/y)
+print('x // y =',x//y)
+print('x ** y =',x**y)
+
+"""output  enter value of x=20
+           enter value of y=10
+            x+y=30
+            x-y=10
+            x*y=200
+            x/y=2.0
+            x//y=2
+            x**y=102400000    """
+
