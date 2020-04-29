13598ef65feec93c04bc065390fa5b8e83825ab9
firoj557 <firojkumar557@gmail.com>
2020-04-29 21:25:45 +0530

    output format pgoram added on repo

---
+++ output format using keyword.py
@@ -0,0 +1,10 @@
+""" Author: Firoj Kumar
+
+ Date: 29-04-2020
+
+This program is output formate print keyword arguments"""
+
+print('this is {name} mobile no,{number}'.format(name='firoj',number=1234567890))
+
+
+"""=this is firoj mobile no=123456789 """
