d42630ba7cf23df24c969eea4929f86335a01694
firoj557 <firojkumar557@gmail.com>
2020-04-29 21:27:05 +0530

    output format oprations program added on repo

---
+++ output format oprations.py
@@ -0,0 +1,15 @@
+""" Author: Firoj Kumar
+
+ Date: 29-04-2020
+
+This program is output formate oprations  program !"""
+
+a=int(input('enter value of a'))
+b=int(input('enter value of b'))
+
+print('mutiplication is {} and {} is ={}'.format(a,b,int(a*b)))
+
+
+"""output enter value of a=5
+          enter value of b=2
+          multiplication is 5 and 2 is=10  """
