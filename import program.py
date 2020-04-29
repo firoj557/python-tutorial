fa8ff61ab01014667a6a8d9813a15850cb365fab
firoj557 <firojkumar557@gmail.com>
2020-04-29 21:30:23 +0530

    impport program added on repo

---
+++ import program.py
@@ -0,0 +1,16 @@
+""" Author: Firoj Kumar
+
+ Date: 29-04-2020
+
+This program is import keyword  program !"""
+
+x=int(input("enter value of x="))
+
+import math
+
+print("x sqrt is =",math.sqrt(x))
+
+"""output enetr value of x =5 
+          x sqrt is= 2.2360797749979 """
+
+
