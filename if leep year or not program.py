c80f811ef9fe0c5168a20fd900b2f099ef155999
firoj557 <firojkumar557@gmail.com>
2020-05-01 21:16:01 +0530

    if leep year program added on repo

---
+++ if leep year or not program.py
@@ -0,0 +1,16 @@
+""" Author: Firoj Kumar
+
+ Date: 01-05-2020
+
+This program is check year is leep year or not  program !"""
+
+x=int(input("enter year="))
+
+if x%4==0:
+    print(x," is leep year")
+else:
+    print(x," is not leep year")
+
+"""output enter year = 2001
+          
+           2001 is not leep year """
