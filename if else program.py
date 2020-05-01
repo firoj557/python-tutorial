0bf2464c8ef46d9015b2532477f2a7bef3539dc7
firoj557 <firojkumar557@gmail.com>
2020-05-01 21:04:27 +0530

    if else program added on repo

---
+++ if else program.py
@@ -0,0 +1,17 @@
+""" Author: Firoj Kumar
+
+ Date: 01-05-2020
+
+This program is if..else statement  program !"""
+
+x=int(input("enter value of x="))
+y=int(input("enter value of y="))
+
+if x>y:
+    print("x is grater",x)
+else:
+    print("y is grater ",y)
+
+"""output enter value of x = 20
+          enter value of y = 30 
+          y is grater              """
