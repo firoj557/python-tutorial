b742067f62ba601b301022b37f79f08d05c36bf8
firoj557 <firojkumar557@gmail.com>
2020-05-01 21:09:52 +0530

    if elif else statment program added on repo

---
+++ if elif else statment program.py
@@ -0,0 +1,25 @@
+""" Author: Firoj Kumar
+
+ Date: 01-05-2020
+
+This program is if..elif...else statement  program !"""
+
+x=int(input("enter day no-"))
+
+if x==1:
+    print(x,"the day is monday")
+elif x==2:
+    print(x,"the day is tusday")
+elif x==3:
+    print(x,"the day is wednesday")
+elif x==4:
+    print(x,"the day is thusday")
+elif x==5:
+    print(x,"the day is friday")
+elif x==6:
+    print(x,"the day is sturday")
+elif x==7:
+    print(x,"the day is sunday")
+else:
+    print(x,"invalid day no")
+
