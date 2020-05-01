367288c5e2b9a8d2fcdd256c0c4d06817ed23cac
firoj557 <firojkumar557@gmail.com>
2020-05-01 21:12:13 +0530

    if else statment program added on repo

---
+++ if else statment program.py
@@ -0,0 +1,16 @@
+""" Author: Firoj Kumar
+
+ Date: 01-05-2020
+
+This program is if..else statement  program !"""
+
+x=int(input("enter value of x="))
+
+
+if x>0:
+    print("x is possitive no=",x)
+else:
+    print("x is negative no=",x)
+
+"""output enter value of x=-1
+           x is negative no=-1"""
