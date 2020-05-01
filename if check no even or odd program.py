12634abcb6c1aa6b29a79e0c7b7f697c3ea96cb3
firoj557 <firojkumar557@gmail.com>
2020-05-01 21:06:21 +0530

    check even or odd no program added on repo

---
+++ if check no even or odd program.py
@@ -0,0 +1,14 @@
+""" Author: Firoj Kumar
+
+ Date: 01-05-2020
+
+This program is check no even or odd  program !"""
+
+x=int(input("enter no="))
+
+if x%2==0:
+    print("no is even")
+else:
+    print("no is odd")
+  """ output enter no=10
+            10 no is even  """
