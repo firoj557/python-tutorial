2df6b9e7bcedcc05e3debcd76a5d0299661543b7
firoj557 <firojkumar557@gmail.com>
2020-05-01 21:17:38 +0530

    if check no positive or negative program added on repo

---
+++ if statement check no possitive or negative.py
@@ -0,0 +1,17 @@
+""" Author: Firoj Kumar
+
+ Date: 01-05-2020
+
+This program is check number positive or negative statement  program !"""
+
+a=int(input("enter  no="))
+
+if (a>0):
+    print("no is possitive")
+elif (a==0):
+    print("no is zero")
+else:
+    print("no is negative")
+
+"""output enter no = -1
+   no is negative    """
