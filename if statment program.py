13947aa467de2fdeb5775da062bb9bc583075c06
firoj557 <firojkumar557@gmail.com>
2020-05-01 21:02:07 +0530

    if statment program added on repo

---
+++ if statment program.py
@@ -0,0 +1,16 @@
+""" Author: Firoj Kumar
+
+ Date: 01-05-2020
+
+This program is if statement  program !"""
+
+x=int(input("enter value of x="))
+
+if x>0:
+    print("X is possitive no.",x)
+print("this is always printed")
+
+"""output 
+        enter value of x= 50
+        x is possitive no .50
+        this is always printed """
