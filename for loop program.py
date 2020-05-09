c87eeb6fb009f6ec857644d2cc36712aad6188ea
firoj557 <firojkumar557@gmail.com>
2020-05-09 22:56:38 +0530

    for loop program added on repo

---
+++ for loop program.py
@@ -0,0 +1,22 @@
+""" Author: Firoj Kumar
+
+ Date: 07-05-2020
+
+This program is for loop !"""
+
+age=[20,30,40,50,60,70,80,90]
+
+k=0
+
+for i in age:
+    print(i)
+
+"""output= 20
+           30 
+           40
+           50
+           60
+           70
+           80
+           90
+              """
