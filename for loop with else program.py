a0bb885a5262b133b999050a1a4e5dc40c5bdb34
firoj557 <firojkumar557@gmail.com>
2020-05-09 23:44:40 +0530

    for loop with else program added on repo

---
+++ for loop with else program.py
@@ -0,0 +1,26 @@
+""" Author: Firoj Kumar
+
+ Date: 08-05-2020
+
+This program is for loop with else  !"""
+
+month=['jan','feb','march','april','may','jun','july','agust','september','oct','nov','dec']
+
+for i in month:
+    print(i)
+else:
+    print("End of month")
+
+"""output jan
+          feb
+          march
+          april
+          may
+          jun
+          july
+          aug
+          sep
+          oct
+          nov
+          dec
+          End of month"""
