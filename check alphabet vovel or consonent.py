3cee33b9646fb42aef52657ecb8666ec09fc5051
firoj557 <firojkumar557@gmail.com>
2020-05-09 22:25:09 +0530

    check alphabet vovel or consonent program

---
+++ check alphabet vovel or consonent.py
@@ -0,0 +1,21 @@
+""" Author: Firoj Kumar
+
+ Date: 02-05-2020
+
+This program is check enter alphabet vovel or consonent  !"""
+
+x=input("enter a alphabet charactor =")
+
+if (x=='a' or x=='e' or x=='i' or x=='o' or x=='u'):
+
+    print(x,"is vovel ")
+
+else:
+    print(x,"is consonent ")
+
+
+"""output enter a alphabet charactor = i
+          i is vovel
+          
+          enter a alphabet charactor = d
+          d is consonent     """
