4d2f9dfc2b18bdd1c4bd850bc9c33d376abb1194
firoj557 <firojkumar557@gmail.com>
2020-05-09 23:37:56 +0530

    table calculate program added on repo program

---
+++ table to be calculated program.py
@@ -0,0 +1,27 @@
+""" Author: Firoj Kumar
+
+ Date: 09-05-2020
+
+This program find table to be calculated  !"""
+
+x=int(input("enter your number="))
+i=0
+t=1
+while i <x:
+    i=i+1
+    t=i*x
+    print('{}x{}  ={}'.format(x,i,t))
+
+    """output enetr your number=10
+            10x1 =10
+            10x2  =20
+            10x3  =30
+            10x4  =40
+            10x5  =50
+            10x6  =60
+            10x7  =70
+            10x8  =80
+            10x9  =90
+            10x10 =100
+            """
+
