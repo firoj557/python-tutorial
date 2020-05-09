f8e43486c280ddde42a4748849bb459f5733e358
firoj557 <firojkumar557@gmail.com>
2020-05-09 23:41:59 +0530

    while loop program added on repo

---
+++ while loop program.py
@@ -0,0 +1,27 @@
+""" Author: Firoj Kumar
+
+ Date: 08-05-2020
+
+This program is while loop program  !"""
+
+x=int(input("enter your no="))
+sum=0
+i=1
+while i<=x:
+    sum=sum+i
+    i=i+1
+    print('the sum of  ={}'.format(sum))
+
+"""output enter your no=10
+           the sum of 10=1
+           the sum of 10=3
+           the sum of 10=6
+           the sum of 10=10
+           the sum of 10=15
+           the sum of 10=21
+           the sum of 10=28
+           the sum of 10=36
+           the sum of 10=45
+           the sum of 10=55
+           
+           """
