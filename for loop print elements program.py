4acb580c08ec4e99e8c794574d1077c292f0d85f
firoj557 <firojkumar557@gmail.com>
2020-05-09 23:29:26 +0530

    for loop print elements program added on repo

---
+++ for loop print elements program.py
@@ -0,0 +1,33 @@
+""" Author: Firoj Kumar
+
+ Date: 08-05-2020
+
+This program is for loop print elements !"""
+
+list=['firoj','rakesh','hitesh',20,50]
+
+for i in range(len(list)):
+    print("list element is=",list[i])
+
+tuple=(20,30,40,50,60,70,80,90,100)
+
+for i in range(len(tuple)):
+
+    print("tuple elements is=",tuple[i])
+
+
+
+"""output  list element is = firoj
+           list element is = rakesh
+           list element is = hitesh
+           list element is = 20
+           list element is = 50
+           tuple elements is=20
+           tuple elements is=30
+           tuple elements is=40
+           tuple elements is=50
+           tuple elements is=60
+           tuple elements is=70
+           tuple elements is=80
+           tuple elements is=90
+           tuple elements is=100"""
