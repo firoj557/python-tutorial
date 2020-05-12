5e6486c365c4f9791f08b7f75918c05a8f2e572e
firoj557 <firojkumar557@gmail.com>
2020-05-12 16:02:00 +0530

    display the cube of given no program added on repo

---
+++ display cube of number program.py
@@ -0,0 +1,20 @@
+""" Author: Firoj Kumar
+
+ Date: 09-05-2020
+
+This program find cube of given number  !"""
+
+x=int(input("enter your number="))
+i=0
+cub=1
+while i<x:
+  i=i+1
+  cub=i**3
+  print('{} the cube is givan number = {}'.format(i,cub))
+
+  """output  enetr your no =5
+             1 the cube is given number=1
+             2 the cube is given number=8
+             3 the cube is given number=27
+             4 the cube is given number=64
+             1 the cube is given number=125"""
