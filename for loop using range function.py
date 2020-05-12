f2ce7fac25fb5613aa57532153dddc1a729206e0
firoj557 <firojkumar557@gmail.com>
2020-05-12 18:07:13 +0530

    for loop using range function program added on repo

---
+++ for loop using range function.py
@@ -0,0 +1,20 @@
+""" Author: Firoj Kumar
+
+ Date: 08-05-2020
+
+This program is for loop using range()  !"""
+
+for i in range(2,20,2):
+    print(i)
+    
+
+"""output 2
+          4
+          6
+          8
+          10
+          12
+          14
+          16
+          18
+          """
