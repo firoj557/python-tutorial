1deefb686b44ab46f8a102ddbc463a01d7dccb15
firoj557 <firojkumar557@gmail.com>
2020-05-12 17:09:09 +0530

    print pattern right angle using ashetrik program added on repo

---
+++ print pattern right angle traingle using an asterik program.py
@@ -0,0 +1,16 @@
+""" Author: Firoj Kumar
+
+ Date: 10-05-2020
+
+This program print pattern  !"""
+
+x=4
+for row in range(1,x+1):
+    for column in range(1,row+1):
+        print("*",end=" ")
+    print( )
+
+"""outpur   *
+            **
+            ***
+            ****   """
