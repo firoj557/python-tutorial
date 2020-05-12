09800886793316f3e9ee1d4efc1d394af4e118c5
firoj557 <firojkumar557@gmail.com>
2020-05-12 15:58:43 +0530

    calculate the factorial of given no program added on repo

---
+++ calculate the factorial program.py
@@ -0,0 +1,20 @@
+""" Author: Firoj Kumar
+
+ Date: 10-05-2020
+
+This program calculate the factorial given no"""
+
+x=int(input("enter your no="))
+y=1
+for i in range(x):
+    i=i+1
+    y=y*i
+    print(y)
+
+
+"""output  enter your no=5
+            1
+            2
+            6
+            24
+            120"""
