4e66bc930de36bc1fa0f068875a46d5fd5b0999f
firoj557 <firojkumar557@gmail.com>
2020-05-23 22:19:37 +0530

    convert decimal into binary program added on repo

---
+++ convert decimal no into binary program.py
@@ -0,0 +1,16 @@
+""" Author: Firoj Kumar
+
+ Date: 21-05-2020
+
+This program is convert decimal no into binary !"""
+
+x=int(input("enter your decimal no="))
+
+convert=x
+print("convert to binary=",bin(x))
+
+
+
+"""output 
+      enter your decimal no=25
+      convert to binary=0b 11001"""
